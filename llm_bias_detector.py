46761fe59cfd9ace55e20fb7da800bd14aed06e0
Liam Magee <liam.magee@gmail.com>
2023-08-15 14:41:06 +1000

    updated system prompt

--- llm_bias_detector.py
+++ llm_bias_detector.py
@@ -17,7 +17,7 @@ tokenizer = AutoTokenizer.from_pretrained('gpt2')
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-prompt_sys = 'You are a qualitative researcher who is an expert in applying the following thematic codes to textual content: Emotional and Psychological Strain; Financial Inconsistencies and Challenges; Mistrust and Skepticism; Repayment and Financial Rectification; Communication and Miscommunication. For output, for each theme you indicate its presence or absence with a zero or one.'
+prompt_sys = 'You are a qualitative researcher working in digital media studies. Your current research project involves going through testimony of the highly public Royal Commission on the Australian Government Robodebt scandal. Take on the role of an expert qualitative researcher, who is performing thematic analysis on a data transcript. You are parsing through excerpts of the data and reviewing it on the basis of pre-defined themes. These are: 1: Emotional and psychological strain; 2: Financial inconsistencies and challenges; 3: Mistrust and skepticism; 4: institutional practices and responsiveness; 5: repayment and financial rectification: 6; communication and miscommunication. For output, for each theme you indicate its presence or absence with a zero or one, as tab-delimited output.'
 prompt = 'After I cancelled my payment they paid me extra money, I was actually entitled to it but they tried to say it was a debt they also tried to pay me money I was not entitled to and refused to stop the payment (even though I was asking them to stop the payment before it happened).'
 messages = []
 messages.append({"role": "system", "content": prompt_sys})
